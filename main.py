d1595d69d9860189a7616294dd852888e9fdcb73
Lenin Acosta <rkb.lenin@gmail.com>
2023-06-09 00:21:45 -0500

    aplicar ruido, descargar, ajustar img

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import cv2
 import numpy as np
 import time
 import os 
+from scipy.ndimage import median_filter
 
 
 class VentanaPrincipal(QMainWindow): 
@@ -12,13 +13,26 @@ class VentanaPrincipal(QMainWindow):
         super().__init__()
         uic.loadUi('gui.ui', self)
         self.pushButton_cargar_img.clicked.connect(self.seleccionar_img)
+        self.pushButton_aplicar_ruido.clicked.connect(self.aplicar_ruido)
+        self.pushButton_python_aplicar.clicked.connect(self.aplicar_python)
+        self.pushButton_python_descargar.clicked.connect(self.descargar_python_espacio)
+        self.pushButton_descargar_img_ruido.clicked.connect(self.descargar_ruido)
+        self.radioButton_img_original.setChecked(False)
+        self.radioButton_img_ruido.setChecked(False)
         self.img_cargada = False
+        self.img_ruido_cargada = False
+        self.img_python_cargada = False
+
     
     def resizeEvent(self, event):
         super().resizeEvent(event)
         if self.img_cargada:
-            self.ajustar_img2label(self.imgpixmap_original, self.label_img_original, 2, 2)
-
+            self.ajustar_img2label(self.pixmap_original, self.label_img_original)
+        if self.img_ruido_cargada:
+            self.ajustar_img2label(self.pixmap_ruido, self.label_img_ruido)
+        if self.img_python_cargada:
+            self.ajustar_img2label(self.pixmap_python_resultado, self.label_python_img)
+            
 
     def seleccionar_img(self):
         archivo = QFileDialog()
@@ -32,26 +46,88 @@ class VentanaPrincipal(QMainWindow):
     def mostrar_img(self, ruta):
         ruta_absoluta = os.path.abspath(ruta)
         ruta_normalizada = os.path.normpath(ruta_absoluta)
-        self.imgpixmap_original = QPixmap(ruta_normalizada)
-        self.ajustar_img2label(self.imgpixmap_original, self.label_img_original, 0, 0)
-        self.mostrar_datos_label(self.label_img_original_datos, self.imgpixmap_original.width(), self.imgpixmap_original.height())
+        self.pixmap_original = QPixmap(ruta_normalizada)
+        self.ajustar_img2label(self.pixmap_original, self.label_img_original)
+        self.mostrar_datos_label(self.label_img_original_datos, self.pixmap_original)
         self.img_cargada = True
-    
-    def mostrar_datos_label(self, label, ancho, alto):
-        label.setText(f'Ancho: {ancho}px ~ Alto: {alto}px')
+        self.radioButton_img_original.setChecked(True)
+        self.img_original = cv2.imread(ruta_normalizada)
+        self.img_original = cv2.cvtColor(self.img_original, cv2.COLOR_BGR2RGB)
+        self.img_trabajo = self.img_original
 
-    def ajustar_img2label(self, imgpixmap, label, margeny, margenx):
-        if imgpixmap.height() > imgpixmap.width():
-            imgpixmap = imgpixmap.scaledToHeight(label.height()-margeny)
+    def mostrar_datos_label(self, label=None, pixmap=None, procesando=False):
+        if not procesando:
+            label.setText(f'Ancho: {pixmap.width()}px ~ Alto: {pixmap.height()}px')
         else:
-            imgpixmap = imgpixmap.scaledToWidth(label.width()-margenx)
+            label.setText('procesando imagen...')
+        QApplication.processEvents()
+
+    def ajustar_img2label(self, imgpixmap, label):
+        imgpixmap = imgpixmap.scaled(label.size(), aspectRatioMode=True)
         label.setPixmap(imgpixmap)
     
-    def cambio_tamanio(self):
-        self.ajustar_img2label(self.imgpixmap_original, self.label_img_original, 2, 2)
+    def aplicar_ruido(self):
+        ruido = np.zeros(self.img_original.shape, dtype=np.uint8)
+        media = self.doubleSpinBox_media.value()
+        desviacion = self.doubleSpinBox_desviacion.value()
+        cv2.randn(ruido, media, desviacion)
+        self.img_ruido = cv2.add(self.img_original, ruido)
+        self.mostrar_img_ruido()
+        self.img_ruido_cargada = True
+        self.cambio_img_trabajo()
 
+    def mostrar_img_ruido(self):
+        self.pixmap_ruido = self.imgcv2pixmap(self.img_ruido)
+        self.ajustar_img2label(self.pixmap_ruido, self.label_img_ruido)
+        self.mostrar_datos_label(self.label_datos_img_ruido, self.pixmap_ruido)
 
+    def imgcv2pixmap(self, img):
+        altoimg, anchoimg, channels = img.shape
+        bytes_linea = channels * anchoimg
+        q_image = QImage(img.data.tobytes(), anchoimg, altoimg, bytes_linea, QImage.Format_RGB888)
+        pixmap = QPixmap.fromImage(q_image)
+        return pixmap
 
+    def cambio_img_trabajo(self):
+        self.radioButton_img_original.setChecked(
+            not self.radioButton_img_original.isChecked()
+        )
+        self.radioButton_img_ruido.setChecked(
+            not self.radioButton_img_ruido.isChecked()
+        )
+        self.img_trabajo = self.img_original if self.radioButton_img_original.isChecked() else self.img_ruido
+    
+    def aplicar_python(self):
+        if self.radioButton_python_espacio.isChecked():
+            self.mostrar_datos_label(label=self.label_python_datos_img, procesando=True)
+            ancho = self.spinBox_python_espacio_ancho.value()
+            alto = self.spinBox_python_espacio_alto.value()
+            r, g, b = cv2.split(self.img_trabajo)
+            r = median_filter(r, size=(ancho, alto))
+            g = median_filter(g, size=(ancho, alto))
+            b = median_filter(b, size=(ancho, alto))
+            self.img_python_resultado = cv2.merge([r,g,b])
+            self.pixmap_python_resultado = self.imgcv2pixmap(self.img_python_resultado)
+            self.ajustar_img2label(self.pixmap_python_resultado, self.label_python_img)
+            self.mostrar_datos_label(self.label_python_datos_img, self.pixmap_python_resultado)
+            self.img_python_cargada = True
+    
+    def  descargar_img(self, img, nombre):
+        directorio = QFileDialog.getExistingDirectory(ventana, 'Seleccionar directorio')
+        if directorio:
+            idtiempo = int(time.time())
+            titulo = f'/{idtiempo}_{nombre}_img.jpg'
+            archivo = directorio + titulo
+            img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+            cv2.imwrite(archivo,img)
+
+    def descargar_python_espacio(self):
+        self.descargar_img(self.img_python_resultado, 'python_espacio')
+
+    def descargar_ruido(self):
+        self.descargar_img(self.img_ruido, 'ruido_agregado')
+
+    
 app = QApplication([])
 ventana = VentanaPrincipal()
 ventana.show()
