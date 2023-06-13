4e019b08c48b4ced69f877ab9186d1ccc1d5cf3c
Lenin Acosta <rkb.lenin@gmail.com>
2023-06-12 23:41:19 -0500

    adapto main a gui

--- main.py
+++ main.py
@@ -12,21 +12,15 @@ from scipy.ndimage import median_filter
 class VentanaPrincipal(QMainWindow):
     def __init__(self):
         super().__init__()
-        uic.loadUi("gui.ui", self)
+        uic.loadUi("gui2.ui", self)
         self.pushButton_cargar_img.clicked.connect(self.seleccionar_img)
         self.pushButton_aplicar_ruido.clicked.connect(self.aplicar_ruido)
-        self.pushButton_python_aplicar.clicked.connect(self.aplicar_python)
-        self.pushButton_python_descargar.clicked.connect(self.descargar_python)
+        self.pushButton_aplicar_filtro.clicked.connect(self.aplicar_filtro)
+        self.pushButton_descargar_resultado.clicked.connect(self.descargar_resultado)
         self.pushButton_descargar_img_ruido.clicked.connect(self.descargar_ruido)
         # 
-        self.radioButton_img_original.setChecked(False)
-        self.radioButton_python_altos.setChecked(True)
-        self.radioButton_img_ruido.setChecked(False)
+        self.img_original_activa = False
         # 
-        self.pushButton_python_aplicar.setEnabled(False)
-        self.pushButton_python_descargar.setEnabled(False)
-        self.pushButton_descargar_img_ruido.setEnabled(False)
-        self.pushButton_aplicar_ruido.setEnabled(False)
         # 
         self.img_cargada = False
         self.img_ruido_cargada = False
@@ -60,12 +54,11 @@ class VentanaPrincipal(QMainWindow):
         # 
         self.img_original = cv2.imread(ruta_normalizada)
         self.img_original = cv2.cvtColor(self.img_original, cv2.COLOR_BGR2RGB)
-        self.img_trabajo = self.img_original
+        self.img_original_activa = True
+        self.cambio_img_trabajo()
         # 
         self.img_cargada = True
-        self.radioButton_img_original.setChecked(True)
         self.pushButton_aplicar_ruido.setEnabled(True)
-        self.pushButton_python_aplicar.setEnabled(True)
 
     def mostrar_datos_label(self, label=None, pixmap=None, procesando=False, mns=None):
         if not procesando:
@@ -88,6 +81,7 @@ class VentanaPrincipal(QMainWindow):
         self.img_ruido = cv2.add(self.img_original, ruido)
         self.mostrar_img_ruido()
         self.img_ruido_cargada = True
+        self.img_original_activa = False
         self.cambio_img_trabajo()
 
     def mostrar_img_ruido(self):
@@ -105,54 +99,49 @@ class VentanaPrincipal(QMainWindow):
         return pixmap
 
     def cambio_img_trabajo(self):
-        self.radioButton_img_original.setChecked(
-            not self.radioButton_img_original.isChecked()
-        )
-        self.radioButton_img_ruido.setChecked(
-            not self.radioButton_img_ruido.isChecked()
-        )
         self.img_trabajo = (
             self.img_original
-            if self.radioButton_img_original.isChecked()
+            if self.img_original_activa
             else self.img_ruido
         )
+
+    def aplicar_filtro(self):
+        if self.radioButton_manual.isChecked():
+            self.aplicar_manual()
+        elif self.radioButton_python.isChecked():
+            self.aplicar_python()
+        
+    def aplicar_manual(self):
+        print()
 
     def aplicar_python(self):
         self.img_python_cargada = False
         self.mostrar_datos_label(label=self.label_python_datos_img, procesando=True)
-        if self.radioButton_python_espacio.isChecked():
-            ancho = self.spinBox_python_espacio_ancho.value()
-            alto = self.spinBox_python_espacio_alto.value()
+        if self.radioButton_espacio.isChecked():
+            ancho = self.spinBox_filtro_x.value()
+            alto = self.spinBox_filtro_y.value()
             r, g, b = cv2.split(self.img_trabajo)
             r = median_filter(r, size=(ancho, alto))
             g = median_filter(g, size=(ancho, alto))
             b = median_filter(b, size=(ancho, alto))
-            self.img_python_resultado = cv2.merge([r, g, b])
-            self.pixmap_python_resultado = self.imgcv2pixmap(self.img_python_resultado)
-            self.ajustar_img2label(self.pixmap_python_resultado, self.label_python_img)
-            self.mostrar_datos_label(self.label_python_datos_img, self.pixmap_python_resultado)
-            self.img_python_cargada = True
-            self.pushButton_python_descargar.setEnabled(True)
-        elif self.radioButton_python_frecuencia.isChecked():
-            if self.radioButton_python_altos.isChecked():
-                radio = self.spinBox_python_frecuencia_radio.value()
-                radio = self.verificar_radio(radio,self.img_trabajo,self.spinBox_python_frecuencia_radio)
-                r, g, b = cv2.split(self.img_trabajo)
-                if self.radioButton_python_altos.isChecked():
-                    r = self.python_filtro_altos(r, radio)
-                    g = self.python_filtro_altos(g, radio)
-                    b = self.python_filtro_altos(b, radio)
-                elif self.radioButton_python_altos.isChecked():
-                    r = self.python_filtro_altos(r, radio)
-                    g = self.python_filtro_altos(g, radio)
-                    b = self.python_filtro_altos(b, radio)
-                self.img_python_resultado = cv2.merge([r,g,b])
-                self.pixmap_python_resultado = self.imgcv2pixmap(self.img_python_resultado)
-                self.ajustar_img2label(self.pixmap_python_resultado, self.label_python_img)
-                self.mostrar_datos_label(self.label_python_datos_img, self.pixmap_python_resultado)
-                self.img_python_cargada = True
-                self.pushButton_python_descargar.setEnabled(True)
-
+        elif self.radioButton_frecuencia.isChecked():
+            radio = self.spinBox_filtro_frecuencia.value()
+            radio = self.verificar_radio(radio,self.img_trabajo,self.spinBox_filtro_frecuencia)
+            r, g, b = cv2.split(self.img_trabajo)
+            if self.radioButton_altos.isChecked():
+                r = self.python_filtro_altos(r, radio)
+                g = self.python_filtro_altos(g, radio)
+                b = self.python_filtro_altos(b, radio)
+            elif self.radioButton_bajos.isChecked():
+                r = self.python_filtro_bajos(r, radio)
+                g = self.python_filtro_bajos(g, radio)
+                b = self.python_filtro_bajos(b, radio)
+        self.img_python_resultado = cv2.merge([r, g, b])
+        self.pixmap_python_resultado = self.imgcv2pixmap(self.img_python_resultado)
+        self.ajustar_img2label(self.pixmap_python_resultado, self.label_python_img)
+        self.mostrar_datos_label(self.label_python_datos_img, self.pixmap_python_resultado)
+        self.img_python_cargada = True
+        self.pushButton_descargar_resultado.setEnabled(True)
 
     def python_filtro_altos(self, img, radio):
         fourier = np.fft.fft2(img)
@@ -161,6 +150,18 @@ class VentanaPrincipal(QMainWindow):
         centro_filas, centro_columnas = filas // 2, columnas // 2
         mascara = np.zeros((filas, columnas), np.uint8)
         mascara[centro_filas - radio:centro_filas + radio, centro_columnas - radio:centro_columnas + radio] = 1
+        filtrado = fdesplazado * mascara
+        inversa_desplazado = np.fft.ifftshift(filtrado)
+        img = np.abs(np.fft.ifft2(inversa_desplazado))
+        return img.astype(np.uint8)
+    
+    def python_filtro_bajos(self, img, radio):
+        fourier = np.fft.fft2(img)
+        fdesplazado = np.fft.fftshift(fourier)
+        filas, columnas = img.shape
+        centro_filas, centro_columnas = filas // 2, columnas // 2
+        mascara = np.ones((filas, columnas), np.uint8)
+        mascara[centro_filas - radio:centro_filas + radio, centro_columnas - radio:centro_columnas + radio] = 0
         filtrado = fdesplazado * mascara
         inversa_desplazado = np.fft.ifftshift(filtrado)
         img = np.abs(np.fft.ifft2(inversa_desplazado))
@@ -185,15 +186,18 @@ class VentanaPrincipal(QMainWindow):
             img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
             cv2.imwrite(archivo, img)
 
-    def descargar_python(self):
-        self.descargar_img(
-            self.img_python_resultado,
-            "python_espacio"
-            if self.radioButton_python_espacio.isChecked()
-            else "python_frecuencia_altos" 
-                if self.radioButton_python_altos.isChecked()
-                else "python_frecuencia_bajos",
-        )
+    def descargar_resultado(self):
+        if self.radioButton_manual.isChecked():
+            print()
+        elif self.radioButton_python.isChecked():
+            self.descargar_img(
+                self.img_python_resultado,
+                "python_espacio"
+                if self.radioButton_espacio.isChecked()
+                else "python_frecuencia_altos" 
+                    if self.radioButton_altos.isChecked()
+                    else "python_frecuencia_bajos",
+            )
 
     def descargar_ruido(self):
         self.descargar_img(self.img_ruido, "ruido_agregado")
